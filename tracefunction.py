6ffa83f1bd0afa14368f4c03b471ee5890211b5f
Fedorova Iana <neku@MacBook-Pro-Fedorova.local>
2019-03-12 18:12:45 +0300

    sys.settrace added

--- tracefunction.py
+++ tracefunction.py
@@ -1,7 +1,19 @@
 import sys
 
+
+
 def tracefunc(frame, event, arg):
     if event == "return":
-        print("function: ", frame.f_code.co_name,", local vars: ", list(frame.f_locals.keys()))
+        print("function: ", frame.f_code.co_name, ", local vars: ", list(frame.f_locals.keys()))
     return tracefunc
+
+def foo():
+    friends = ["Bob", "Tom"]
+
+    for f in friends:
+        print("Hi %s!” % f")
+    return len(friends)
+
+sys.settrace(tracefunc)
+foo()
 
